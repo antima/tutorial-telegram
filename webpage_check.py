456fd08efb53211cbe64bfe45449f955e6ffb9c2
mar <gim.marcello@gmail.com>
2021-03-23 22:39:16 +0100

    webpage checker example skeleton

---
+++ webpage_check.py
@@ -0,0 +1,20 @@
+import requests
+
+target_url = ""
+older = ""
+
+
+def page_updated():
+    """
+    Controlla che la pagina sia cambiata dalla scorso volta
+    :return: True se la pagina sotto controllo ha avuto modifiche, altrimenti False
+    """
+    global older
+    if not older:
+        older = requests.get(target_url).text
+    page_data = requests.get(target_url).text
+
+    if page_data != older:
+        return True
+    return False
+
